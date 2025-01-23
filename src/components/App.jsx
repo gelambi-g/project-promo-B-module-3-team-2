5f5d6313f410e0ec9019f555cd0be9b4cd93eb2f
gelambi-g <gelambi.g@gmail.com>
2025-01-23 12:13:03 +0100

    !

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
 <header className="header">
   <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
     <img className="header__companyLogo" src="./images/laptop-code-solid.svg" alt="Logo proyectos molones"/>
-    <h1 className="header__title">Proyectos molones</h1>
+    <h1 className="header__title">Proyectos molones!</h1>
   </a>
   <img className="logoSponsor" src="./images/adalab.png" alt="Logo Adalab"/>
 </header>
