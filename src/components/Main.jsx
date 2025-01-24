f0cc049966e1b0576342144909238380aa94d31b
IdoiaBE <1992ibe@gmail.com>
2025-01-24 12:37:19 +0100

    issue29-hero

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,10 @@
+import Hero from "./Hero"
 
 
 function Main() {
   return (
     <main className="main">
-        <section className="hero">
-        <h2 className="title">Proyectos molones</h2>
-        <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
-        <a className="button--link" href="./">Ver proyectos</a>
-        </section>
+        <Hero/>
 
         <section className="preview">
         <div className="projectImage"></div>
