e6a1d8b8a34d7e0f57343322304ea46b1643e05d
gelambi-g <gelambi.g@gmail.com>
2025-01-27 13:55:23 +0100

    landind progress

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,12 +1,22 @@
-
+import Card from "./Card";
+import Footer from "./Footer";
 import Header from "./Header";
+import Hero from "./Hero";
 
 
-function Landing (){
+function Landing (props){
 
     return(
+    <>
         <Header/>
-        
+        <Hero />
+        <Card dataCard={props.dataCard}/>
+        <Card dataCard={props.dataCard}/>
+        <Card dataCard={props.dataCard}/>
+        <Card dataCard={props.dataCard}/>
+
+        <Footer />
+    </>
     )
 }
 
